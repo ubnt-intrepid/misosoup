78891a7b16ac8a3373880679e7613fb1ded7dfa9
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-09-27 19:02:27 +0900

    refactor(index_builder): inner

--- src/index_builder/builder.rs
+++ src/index_builder/builder.rs
@@ -12,28 +12,14 @@ use super::index::StructuralIndex;
 pub struct IndexBuilder<B: Backend> {
     backend: B,
     inner: RefCell<Inner>,
-    level: usize,
 }
-
-#[derive(Debug, Default)]
-pub(crate) struct Inner {
-    pub(crate) bitmaps: Vec<Bitmap>,
-    pub(crate) b_colon: Vec<Vec<u64>>,
-    pub(crate) b_comma: Vec<Vec<u64>>,
-}
-
 
 impl<B: Backend> IndexBuilder<B> {
     #[allow(missing_docs)]
     pub fn new(backend: B, level: usize) -> Self {
         Self {
             backend,
-            inner: RefCell::new(Inner {
-                bitmaps: vec![],
-                b_colon: vec![vec![]; level],
-                b_comma: vec![vec![]; level],
-            }),
-            level,
+            inner: RefCell::new(Inner::new(level)),
         }
     }
 
@@ -62,16 +48,16 @@ impl<B: Backend> IndexBuilder<B> {
             }
 
             // Step 1
-            build_structural_character_bitmaps(&mut inner.bitmaps, record, &self.backend);
+            inner.build_structural_character_bitmaps(record, &self.backend);
 
             // Step 2
-            remove_unstructural_quotes(&mut inner.bitmaps);
+            inner.remove_unstructural_quotes();
 
             // Step 3
-            remove_unstructural_characters(&mut inner.bitmaps);
+            inner.remove_unstructural_characters()?;
 
             // Step 4
-            build_leveled_bitmaps(&mut inner, self.level)?;
+            inner.build_leveled_bitmaps()?;
         }
 
         Ok(StructuralIndex {
@@ -81,47 +67,169 @@ impl<B: Backend> IndexBuilder<B> {
 }
 
 
-
-fn build_structural_character_bitmaps<B: Backend>(bitmaps: &mut Vec<Bitmap>, s: &[u8], backend: &B) {
-    for i in 0..(s.len() / 64) {
-        bitmaps.push(backend.create_full_bitmap(s, i * 64));
-    }
-
-    if s.len() % 64 != 0 {
-        bitmaps.push(backend.create_partial_bitmap(s, (s.len() / 64) * 64));
-    }
+#[derive(Debug, Default)]
+pub(crate) struct Inner {
+    pub(crate) bitmaps: Vec<Bitmap>,
+    pub(crate) b_colon: Vec<Vec<u64>>,
+    pub(crate) b_comma: Vec<Vec<u64>>,
+    level: usize,
 }
 
-fn remove_unstructural_quotes(bitmaps: &mut [Bitmap]) {
-    let mut uu = 0u64;
-    for i in 0..bitmaps.len() {
-        // extract the backslash bitmap, whose succeeding element is a quote.
-        let q1 = bitmaps[i].quote;
-        let q2 = if i + 1 == bitmaps.len() {
-            0
-        } else {
-            bitmaps[i + 1].quote
-        };
-        let mut bsq = (q1 >> 1 | q2 << 63) & bitmaps[i].backslash;
+impl Inner {
+    #[inline]
+    fn new(level: usize) -> Self {
+        Inner {
+            bitmaps: vec![],
+            b_colon: vec![vec![]; level],
+            b_comma: vec![vec![]; level],
+            level,
+        }
+    }
 
-        // extract the bits for escaping a quote from `bsq`.
-        let mut u = 0u64;
-        while bsq != 0 {
-            // The target backslash bit.
-            let target = bit::E(bsq);
-            let pos = 64 - target.leading_zeros();
-            if consecutive_ones(&bitmaps[0..i + 1], pos).is_odd() {
-                u |= target;
-            }
-            bsq ^= target; // clear the target bit.
+    fn build_structural_character_bitmaps<B: Backend>(&mut self, record: &[u8], backend: &B) {
+        for i in 0..(record.len() / 64) {
+            self.bitmaps
+                .push(backend.create_full_bitmap(record, i * 64));
         }
 
-        bitmaps[i].quote &= !(uu >> 63 | u << 1);
+        if record.len() % 64 != 0 {
+            self.bitmaps
+                .push(backend.create_partial_bitmap(record, (record.len() / 64) * 64));
+        }
+    }
 
-        // save the current result for next iteration
-        uu = u;
+    fn remove_unstructural_quotes(&mut self) {
+        let mut uu = 0u64;
+        for i in 0..self.bitmaps.len() {
+            // extract the backslash bitmap, whose succeeding element is a quote.
+            let q1 = self.bitmaps[i].quote;
+            let q2 = if i + 1 == self.bitmaps.len() {
+                0
+            } else {
+                self.bitmaps[i + 1].quote
+            };
+            let mut bsq = (q1 >> 1 | q2 << 63) & self.bitmaps[i].backslash;
+
+            // extract the bits for escaping a quote from `bsq`.
+            let mut u = 0u64;
+            while bsq != 0 {
+                // The target backslash bit.
+                let target = bit::E(bsq);
+                let pos = 64 - target.leading_zeros();
+                if consecutive_ones(&self.bitmaps[0..i + 1], pos).is_odd() {
+                    u |= target;
+                }
+                bsq ^= target; // clear the target bit.
+            }
+
+            self.bitmaps[i].quote &= !(uu >> 63 | u << 1);
+
+            // save the current result for next iteration
+            uu = u;
+        }
+    }
+
+    fn remove_unstructural_characters(&mut self) -> Result<()> {
+        // The number of quotes in structural quote bitmap
+        let mut n = 0;
+
+        for b in &mut self.bitmaps {
+            let mut m_quote = b.quote;
+            let mut m_string = 0u64;
+            while m_quote != 0 {
+                // invert all of bits from the rightmost 1 of `m_quote` to the end
+                m_string ^= bit::S(m_quote);
+                // remove the rightmost 1 from `m_quote`
+                m_quote = bit::R(m_quote);
+                n += 1;
+            }
+
+            if n.is_odd() {
+                m_string ^= !0u64;
+            }
+
+            b.colon &= !m_string;
+            b.comma &= !m_string;
+            b.left_brace &= !m_string;
+            b.right_brace &= !m_string;
+            b.left_bracket &= !m_string;
+            b.right_bracket &= !m_string;
+        }
+
+        if !n.is_even() {
+            Err(ErrorKind::InvalidRecord)?;
+        }
+
+        Ok(())
+    }
+
+    fn build_leveled_bitmaps(&mut self) -> Result<()> {
+        for i in 0..self.level {
+            self.b_colon[i].extend(self.bitmaps.iter().map(|b| b.colon));
+            self.b_comma[i].extend(self.bitmaps.iter().map(|b| b.comma));
+        }
+
+        let mut s = Vec::new();
+        for (i, b) in self.bitmaps.iter().enumerate() {
+            let mut m_left = b.left_brace | b.left_bracket;
+            let mut m_right = b.right_brace | b.right_bracket;
+
+            loop {
+                let m_rightbit = bit::E(m_right);
+                let mut m_leftbit = bit::E(m_left);
+                while m_leftbit != 0 && (m_rightbit == 0 || m_leftbit < m_rightbit) {
+                    let t = m_leftbit & b.left_brace != 0;
+                    s.push((i, m_leftbit, t));
+                    m_left = bit::R(m_left);
+                    m_leftbit = bit::E(m_left);
+                }
+
+                if m_rightbit != 0 {
+                    let (j, mlb, t) = s.pop()
+                        .ok_or_else(|| Error::from(ErrorKind::InvalidRecord))
+                        .chain_err(|| "s.pop()")?;
+                    if t != (m_rightbit & b.right_brace != 0) {
+                        return Err(Error::from(ErrorKind::InvalidRecord)).chain_err(|| "invalid bracket/brace");
+                    }
+                    m_leftbit = mlb;
+
+                    if s.len() > 0 && s.len() - 1 < self.level {
+                        let b_colon = &mut self.b_colon[s.len() - 1];
+                        let b_comma = &mut self.b_comma[s.len() - 1];
+
+                        if i == j {
+                            let mask = !m_rightbit.wrapping_sub(m_leftbit);
+                            b_colon[i] &= mask;
+                            b_comma[i] &= mask;
+                        } else {
+                            let mask = m_leftbit.wrapping_sub(1);
+                            b_colon[j] &= mask;
+                            b_comma[j] &= mask;
+
+                            let mask = !m_rightbit.wrapping_sub(1);
+                            b_colon[i] &= mask;
+                            b_comma[i] &= mask;
+
+                            for k in j + 1..i {
+                                b_colon[k] = 0;
+                                b_comma[k] = 0;
+                            }
+                        }
+                    }
+                }
+
+                m_right = bit::R(m_right);
+
+                if m_rightbit == 0 {
+                    break;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
+
 
 /// Compute the length of the consecutive ones in the backslash bitmap starting at `pos`
 #[inline]
@@ -139,102 +247,6 @@ fn consecutive_ones(b: &[Bitmap], pos: u32) -> u32 {
         ones += 64;
     }
     ones
-}
-
-fn remove_unstructural_characters(bitmaps: &mut [Bitmap]) {
-    // The number of quotes in structural quote bitmap
-    let mut n = 0;
-
-    for b in bitmaps {
-        let mut m_quote = b.quote;
-        let mut m_string = 0u64;
-        while m_quote != 0 {
-            // invert all of bits from the rightmost 1 of `m_quote` to the end
-            m_string ^= bit::S(m_quote);
-            // remove the rightmost 1 from `m_quote`
-            m_quote = bit::R(m_quote);
-            n += 1;
-        }
-
-        if n.is_odd() {
-            m_string ^= !0u64;
-        }
-
-        b.colon &= !m_string;
-        b.comma &= !m_string;
-        b.left_brace &= !m_string;
-        b.right_brace &= !m_string;
-        b.left_bracket &= !m_string;
-        b.right_bracket &= !m_string;
-    }
-
-    debug_assert!(n.is_even());
-}
-
-fn build_leveled_bitmaps(inner: &mut Inner, level: usize) -> Result<()> {
-    for i in 0..level {
-        inner.b_colon[i].extend(inner.bitmaps.iter().map(|b| b.colon));
-        inner.b_comma[i].extend(inner.bitmaps.iter().map(|b| b.comma));
-    }
-
-    let mut s = Vec::new();
-    for (i, b) in inner.bitmaps.iter().enumerate() {
-        let mut m_left = b.left_brace | b.left_bracket;
-        let mut m_right = b.right_brace | b.right_bracket;
-
-        loop {
-            let m_rightbit = bit::E(m_right);
-            let mut m_leftbit = bit::E(m_left);
-            while m_leftbit != 0 && (m_rightbit == 0 || m_leftbit < m_rightbit) {
-                let t = m_leftbit & b.left_brace != 0;
-                s.push((i, m_leftbit, t));
-                m_left = bit::R(m_left);
-                m_leftbit = bit::E(m_left);
-            }
-
-            if m_rightbit != 0 {
-                let (j, mlb, t) = s.pop()
-                    .ok_or_else(|| Error::from(ErrorKind::InvalidRecord))
-                    .chain_err(|| "s.pop()")?;
-                if t != (m_rightbit & b.right_brace != 0) {
-                    return Err(Error::from(ErrorKind::InvalidRecord)).chain_err(|| "invalid bracket/brace");
-                }
-                m_leftbit = mlb;
-
-                if s.len() > 0 && s.len() - 1 < level {
-                    let b_colon = &mut inner.b_colon[s.len() - 1];
-                    let b_comma = &mut inner.b_comma[s.len() - 1];
-
-                    if i == j {
-                        let mask = !m_rightbit.wrapping_sub(m_leftbit);
-                        b_colon[i] &= mask;
-                        b_comma[i] &= mask;
-                    } else {
-                        let mask = m_leftbit.wrapping_sub(1);
-                        b_colon[j] &= mask;
-                        b_comma[j] &= mask;
-
-                        let mask = !m_rightbit.wrapping_sub(1);
-                        b_colon[i] &= mask;
-                        b_comma[i] &= mask;
-
-                        for k in j + 1..i {
-                            b_colon[k] = 0;
-                            b_comma[k] = 0;
-                        }
-                    }
-                }
-            }
-
-            m_right = bit::R(m_right);
-
-            if m_rightbit == 0 {
-                break;
-            }
-        }
-    }
-
-    Ok(())
 }
 
 
