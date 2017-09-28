87425f2bcf105021ad556128134a6ff00b359970
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-09-28 15:30:51 +0900

    refactor

--- src/index_builder/index.rs
+++ src/index_builder/index.rs
@@ -1,10 +1,8 @@
 use std::cell::Ref;
 use bit;
-use errors::{ErrorKind, Result, ResultExt};
+use errors::{ErrorKind, Result};
 use value::EscapedStr;
-
 use super::builder::Inner;
-use super::backend::Bitmap;
 
 
 /// Structural index of a slice of bytes
@@ -38,28 +36,48 @@ impl<'a, 's> StructuralIndex<'a, 's> {
     }
 
     #[allow(missing_docs)]
+    #[inline]
     pub fn find_object_field(&self, begin: usize, end: usize) -> Result<(EscapedStr<'s>, usize)> {
-        let (fsi, fei) =
-            find_pre_field_indices(&self.inner.bitmaps, begin, end).chain_err(|| "find_pre_field_indices()")?;
-        Ok((EscapedStr::from(&self.record[fsi..fei]), fsi))
-    }
+        let mut ei = None;
 
-    #[allow(missing_docs)]
-    pub fn find_object_value(&self, begin: usize, end: usize, is_last_field: bool) -> (usize, usize) {
-        let delim = if is_last_field { b'}' } else { b',' };
-        let (vsi, mut vei) = trimmed(&self.record, begin, end);
-        while vei > begin && self.record.as_bytes()[vei - 1] == delim {
-            vei -= 1;
+        for i in (begin / 64..(end + 1 + 63) / 64).rev() {
+            let mut m_quote = self.inner.bitmaps[i].quote;
+            while m_quote != 0 {
+                let offset = (i + 1) * 64 - (m_quote.leading_zeros() as usize) - 1;
+                if offset < end {
+                    if let Some(ei) = ei {
+                        let si = offset + 1;
+                        return Ok((EscapedStr::from(&self.record[si..ei]), si));
+                    } else {
+                        ei = Some(offset);
+                    }
+                }
+                m_quote = bit::L(m_quote);
+            }
         }
-        trimmed(&self.record, vsi, vei)
+
+        Err(ErrorKind::InvalidRecord.into())
     }
 
     #[allow(missing_docs)]
+    #[inline]
+    pub fn find_object_value(&self, begin: usize, end: usize, is_last_field: bool) -> (usize, usize) {
+        find_object_value(
+            self.record.as_bytes(),
+            begin,
+            end,
+            if is_last_field { b'}' } else { b',' },
+        )
+    }
+
+    #[allow(missing_docs)]
+    #[inline]
     pub fn find_array_value(&self, begin: usize, end: usize) -> (usize, usize) {
-        trimmed(&self.record, begin, end)
+        find_array_value(self.record.as_bytes(), begin, end)
     }
 
     #[allow(missing_docs)]
+    #[inline]
     pub fn substr(&self, begin: usize, end: usize) -> &'s str {
         debug_assert!(begin <= end);
         &self.record[begin..end]
@@ -67,6 +85,7 @@ impl<'a, 's> StructuralIndex<'a, 's> {
 }
 
 
+#[inline]
 fn generate_positions(bitmap: &[u64], begin: usize, end: usize, cp: &mut Vec<usize>) {
     for i in begin / 64..(end - 1 + 63) / 64 {
         let mut m_bits = bitmap[i];
@@ -81,49 +100,81 @@ fn generate_positions(bitmap: &[u64], begin: usize, end: usize, cp: &mut Vec<usi
     }
 }
 
-fn find_pre_field_indices(bitmaps: &[Bitmap], begin: usize, end: usize) -> Result<(usize, usize)> {
-    let mut ei = None;
-
-    for i in (begin / 64..(end + 1 + 63) / 64).rev() {
-        let mut m_quote = bitmaps[i].quote;
-        while m_quote != 0 {
-            let offset = (i + 1) * 64 - (m_quote.leading_zeros() as usize) - 1;
-            if offset < end {
-                if let Some(ei) = ei {
-                    let si = offset + 1;
-                    return Ok((si, ei));
-                } else {
-                    ei = Some(offset);
-                }
-            }
-            m_quote = bit::L(m_quote);
+#[inline]
+fn find_object_value(s: &[u8], mut begin: usize, mut end: usize, delim: u8) -> (usize, usize) {
+    while begin < end {
+        match s[begin] {
+            b' ' | b'\t' | b'\r' | b'\n' => begin += 1,
+            _ => break,
         }
     }
 
-    Err(ErrorKind::InvalidRecord.into())
-}
-
-
-fn trimmed(s: &str, mut begin: usize, mut end: usize) -> (usize, usize) {
-    while begin < end && is_ws(s, begin) {
-        begin += 1;
+    let mut seen_delim = false;
+    while end > begin {
+        match s[end - 1] {
+            b' ' | b'\t' | b'\r' | b'\n' => end -= 1,
+            s if s == delim && !seen_delim => {
+                seen_delim = true;
+                end -= 1;
+            }
+            _ => break,
+        }
     }
-    while end >= begin && is_ws(s, end - 1) {
-        end -= 1;
-    }
+
     (begin, end)
 }
 
-#[test]
-fn trimmed_1() {
-    let s = "[a, b, c]";
-    let (b, e) = trimmed(s, 0, s.len());
-    assert_eq!(&s[b..e], "[a, b, c]");
+#[inline]
+fn find_array_value(s: &[u8], mut begin: usize, mut end: usize) -> (usize, usize) {
+    while begin < end {
+        match s[begin] {
+            b' ' | b'\t' | b'\r' | b'\n' => begin += 1,
+            _ => break,
+        }
+    }
+
+    while end >= begin {
+        match s[end - 1] {
+            b' ' | b'\t' | b'\r' | b'\n' => end -= 1,
+            _ => break,
+        }
+    }
+
+    (begin, end)
 }
 
-fn is_ws(s: &str, i: usize) -> bool {
-    match s.as_bytes()[i] {
-        b' ' | b'\n' | b'\t' => true,
-        _ => false,
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_object_value() {
+        struct TestCase {
+            input: &'static [u8],
+            begin: usize,
+            end: usize,
+            delim: u8,
+            expect: (usize, usize),
+        }
+        let tests = &[
+            TestCase {
+                input: br#"{ "a": {}, "b": [] } "#,
+                begin: 6,
+                end: 10,
+                delim: b',',
+                expect: (7, 9),
+            },
+            TestCase {
+                input: br#"{ "a": {}, "b": [] } "#,
+                begin: 16,
+                end: 21,
+                delim: b'}',
+                expect: (16, 18),
+            },
+        ];
+        for t in tests {
+            let actual = find_object_value(t.input, t.begin, t.end, t.delim);
+            assert_eq!(actual, t.expect);
+        }
     }
 }
